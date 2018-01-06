b71b7b8495c0dfc43a6ce8c351f7ffa7e5e78fee
stephenhky <stephenhky@yahoo.com.hk>
2018-01-06 18:01:51 -0500

    starting

--- stacking.R
+++ stacking.R
@@ -1,1 +1,10 @@
 
+library(dplyr)
+library(ggplot2)
+
+nbdata<- 20000
+nbclasses<- 6
+nbprifeatures<- 5
+binfeatures<- c(5, 6)
+
+data.frame(x=seq(0, 1, 0.1), y=dexp(seq(0, 1, 0.1))) %>% ggplot(aes(x, y)) + geom_point()
