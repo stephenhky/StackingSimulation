232a0a4e8372892261c7f24f75e6f9e5a43cd61b
stephenhky <stephenhky@yahoo.com.hk>
2018-01-07 21:26:34 -0500

    in progress

--- stacking.R
+++ stacking.R
@@ -7,4 +7,4 @@ nbclasses<- 6
 nbprifeatures<- 5
 binfeatures<- c(5, 6)
 
-data.frame(x=seq(0, 1, 0.1), y=dexp(seq(0, 1, 0.1))) %>% ggplot(aes(x, y)) + geom_point()
+data.frame(x=seq(0, 1, 0.01), y=dexp(seq(0, 1, 0.01), rate=5)) %>% ggplot(aes(x, y)) + geom_line()
